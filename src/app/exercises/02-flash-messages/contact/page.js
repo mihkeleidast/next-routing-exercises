a727906c4f6233e9f2f97ed4bf587796a6c48639
Mihkel Eidast <mihkel@eida.st>
2026-01-07 18:48:33 +0200

    exercise 2

--- src/app/exercises/02-flash-messages/contact/page.js
+++ src/app/exercises/02-flash-messages/contact/page.js
@@ -1,10 +1,21 @@
 'use client';
 import React from 'react';
+import { useRouter } from 'next/navigation';
+import { ToastContext } from '../../../../components/ToastProvider';
 
 function ContactPage() {
+  const router = useRouter();
+  const { createToast } = React.useContext(ToastContext)
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    createToast('Got it!', 'success');
+
+    router.push('.')
+  }
   return (
     <main>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="name">Name:</label>
         <input id="name" required={true} />
 
--- src/app/exercises/02-flash-messages/layout.js
+++ src/app/exercises/02-flash-messages/layout.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
+import ToastProvider from '../../../components/ToastProvider'
+
 import './styles.css';
+import ToastShelf from '../../../components/ToastShelf';
 
 function FlashMsgLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+          <ToastProvider>
+            {children}
+            <ToastShelf />
+          </ToastProvider>
+        </body>
     </html>
   );
 }
